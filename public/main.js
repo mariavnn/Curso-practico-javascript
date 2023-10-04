8afd28177530492622421f358d1d2453fd4210a6
mariavnn <marianieto290520@gmail.com>
2023-10-04 12:14:01 -0500

    Componentes HTML y algunas funcionalidades de JS

---
+++ public/main.js
@@ -0,0 +1,44 @@
+// SELECT ITEMS
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.querySelector('#grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//EDIT OPTION
+let editElement;
+let editFlag = false;
+let editID = "";
+
+
+// EVENT LISTENERS
+form.addEventListener('submit', addItem);
+
+//FUNCTIONS
+function addItem(a){
+    a.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime.toString();
+    if(value && !editFlag){
+        console.log("add item");
+    }
+    else if (value && editFlag){
+        console.log("editing");
+    }
+    else{
+        console.log("empty value");
+        
+    }
+}
+
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.add(`alert-${action}`);
+    }, 1000);
+}
